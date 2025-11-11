f11fb371be835a0ab97c211f0b30a0598abf4fbc
nrspoorthy <nrspoorthy@gmail.com>
2025-11-11 10:27:29 +0530

    Added new Vercel domain to CORS whitelist

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://recipe-sharing-community-mern-npqd.vercel.app"
+      "https://recipe-sharing-community-mern-lp98.vercel.app"
     ],
     credentials: true,
   })
